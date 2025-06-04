a43043fe9f16e1ce8f06e73a1bcc11cca331ef14
Leandro Bohacec <leandro_bohacec@live.com>
2025-06-04 18:51:22 -0300

    Otro commit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.get('/buscar', async (req, res) => {
   const query = req.query.producto;
   if (!query) return res.status(400).json({ error: 'Falta el parámetro "producto"' });
 
+  //Cetrogar
   const url = `https://www.cetrogar.com.ar/catalogsearch/result/?q=${encodeURIComponent(query)}`;
 
   try {
@@ -18,14 +19,44 @@ app.get('/buscar', async (req, res) => {
     });
 
     const $ = cheerio.load(response.data);
-    console.log(response)
     const productos = [];
 
-    $('.product-item-info').each((i, el) => {
-      const titulo = $(el).find('.product.name a').text().trim();
-      const precio = $(el).find('.price').first().text().trim();
-      if (titulo) productos.push({ titulo, precio });
-    });
+    if($('.base').text().trim()){
+        //con la descripcion encontro 1 solo resultado. De esta manera la pagina tiene otra estructura
+         const nombre = $('.base').text().trim()
+         const precio = $('span#price-including-tax-product-price-42661 > span.price').text().trim();
+
+         let urlImagenProcesada = ''
+
+         productos.push({
+                origen: 'Cetrogar',
+                nombre:nombre,
+                precio:precio,
+                urlProducto:urlImagenProcesada
+            })
+    }else{
+        //son varios resultados
+        $('li.item.product').each(function() {
+            const nombre = $(this).find('.product-item-name .name-container').text().trim();
+            const precio = $(this).find('.price-final_price .final-price .price').first().text().trim();
+            const urlImagen = $(this).find('.product-image-photo').attr('src');
+            let urlImagenProcesada = ''
+            //const urlProducto = $(this).find('a.product-item-info').attr('href');
+
+            if(urlImagen !== undefined){
+                urlImagenProcesada = urlImagen.split("?")[0]
+            }
+
+            productos.push({
+                origen: 'Cetrogar',
+                nombre:nombre,
+                precio:precio,
+                urlProducto:urlImagenProcesada
+            })
+        });
+    }
+
+    
 
     res.json(productos);
   } catch (error) {
